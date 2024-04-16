02935762ec821eb0f8fd32e5f9bdde5611863a4f
vanessadecoded <vanessamayama@gmail.com>
2024-04-16 21:25:07 +0300

    modified config file

--- _config.js
+++ _config.js
@@ -2,14 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-<<<<<<< HEAD
     production: 'mongodb+srv://<tracymayama>:<mayama123>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<tracymayama>:<mayama123>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://<tracymayama>:<mayama123>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-=======
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
->>>>>>> origin/test
 }
 module.exports = config;
